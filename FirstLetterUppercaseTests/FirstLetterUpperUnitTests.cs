f78c12f310a4caaf3462e8b4f73ea9747f391cb8
Joe Gregoria <jgregoria@gmail.com>
2017-05-10 14:45:01 -0400

    add code and test for null and empty cases.  Also some test cases for non-american characters

--- FirstLetterUppercaseTests/FirstLetterUpperUnitTests.cs
+++ FirstLetterUppercaseTests/FirstLetterUpperUnitTests.cs
@@ -16,5 +16,54 @@ namespace FirstLetterUppercaseTests
         {
             Assert.False(FirstLetterUppercase.IsFirstLetterUppercase("hello"));
         }
+
+        [Fact]
+        public void NullShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase(null));
+        }
+
+        [Fact]
+        public void EmptyShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase(""));
+        }
+
+        [Fact]
+        public void SpaceShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase(" "));
+        }
+
+        [Fact]
+        public void TabShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase("\t"));
+        }
+
+        [Fact]
+        public void SingleCapitalShouldBeTrue()
+        {
+            Assert.True(FirstLetterUppercase.IsFirstLetterUppercase("A"));
+        }
+
+        [Fact]
+        public void SingleLowerShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase("a"));
+        }
+
+        [Fact]
+        public void AccentUpperAShouldBeTrue()
+        {
+            Assert.True(FirstLetterUppercase.IsFirstLetterUppercase("À"));
+        }
+
+
+        [Fact]
+        public void AccentLowerAShouldBeFalse()
+        {
+            Assert.False(FirstLetterUppercase.IsFirstLetterUppercase("à"));
+        }
     }
 }
